9d60c1cefb6b66dea530d7774a04e6494697cc3c
Kayathri-maths <kayualagarsamy@gmail.com>
2026-01-11 21:52:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="pt-24">
+        {" "}
+        {/* Space below fixed header */}
+        {/* Next sections will go here */}
+      </div>
+    </>
+  );
+}
+
+export default App;
